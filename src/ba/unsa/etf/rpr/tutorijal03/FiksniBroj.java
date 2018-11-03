7ebb17312abbba4ab719993bdd57fd89be5082b5
mplakalovi1 <mplakalovi1@etf.unsa.ba>
2018-11-03 20:58:09 +0100

    klasa FiksniBroj uradjena...

--- src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
@@ -1,18 +1,19 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
-/*
-FiksniBroj sadrži konstruktor FiksniBroj(Grad grad, String broj). Parametar broj je dio telefonskog broja bez pozivnog broja npr. "123-456",
-a grad je promjenljiva pobrojanog tipa koji označava pozivni broj koji treba koristiti npr.:
-	enum Grad { SARAJEVO, TUZLA, ZENICA…}
-Spisak pozivnih brojeva se može naći ovdje. Umjesto imena kantona koristite ime glavnog grada kantona (kao u primjeru iznad),
-a umjesto Brčko distrikta stavite BRCKO. Metoda ispisi() treba vratiti broj oblika "033/123-456".
-
- */
-
 public class FiksniBroj extends TelefonskiBroj {
     private String broj;
+    private Grad grad;
+    public FiksniBroj(Grad grad, String broj){ //konstruktor
+        this.broj=broj;
+        this.grad=grad;
+    }
 
-    private enum Grad{ //promjenljive pobrojanog tipa;
+    @Override
+    public String ispisi(){
+        String s=new String(grad.pozivni + "/" + broj);
+        return s;
+    }
+    public enum Grad{ //promjenljive pobrojanog tipa;
         TRAVNIK("030"),
         ORAŠJE("031"),
         ZENICA("032"),
@@ -26,14 +27,9 @@ public class FiksniBroj extends TelefonskiBroj {
         BRČKO("049");
 
         private final String pozivni; //broj pozivni za odg grad;
-         Grad(String pozivni){ //konstruktor za enum, koji nam omogucava pridruzivanje odg. brojeva pobrojanim;
+        Grad(String pozivni){ //konstruktor za enum, koji nam omogucava pridruzivanje odg. brojeva pobrojanim;
             this.pozivni=pozivni;
         }
     }
 
-
-
-    public FiksniBroj(Grad grad, String broj){
-
-    }
 }
