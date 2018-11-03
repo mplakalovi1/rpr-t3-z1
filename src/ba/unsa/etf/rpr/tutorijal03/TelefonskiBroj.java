ee375ada2fa34b88ffcfe8bba010dbf8d5c7f86e
mplakalovi1 <mplakalovi1@etf.unsa.ba>
2018-11-03 19:38:34 +0100

    apstraktna klasa

--- src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
@@ -1,15 +1,6 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
-public abstract class TelefonskiBroj implements Comparable<String> {
+public abstract class TelefonskiBroj {
     public abstract String ispisi();
-
-    @Override
-    public int compareTo(String o) {
-        return this.ispisi().compareTo(o);
-    }
-
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
+    public abstract int hashCode();
 }
